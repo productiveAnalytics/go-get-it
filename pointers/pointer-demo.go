a0530fc2fd76b9b7306e97ff03b6703659be6b12
Chawathe, Lalit <lalit.chawathe@changehealthcare.com>
2019-08-23 17:36:30 -0500

    Updated for address-of operator

--- pointers/pointer-demo.go
+++ pointers/pointer-demo.go
@@ -7,4 +7,17 @@ func main() {
 	var orgName *string = new(string)
 	*orgName = "ProductiveAnalytics"
 	fmt.Println("Dereferencing Pointer: " + *orgName)
+
+	fmt.Println()
+
+	var newName string = "Prod8ctive"
+
+	// address-of operator
+	newOrgNamePtr := &newName
+	fmt.Println("Pointer Address & Content :")
+	fmt.Println(newOrgNamePtr, *newOrgNamePtr)
+
+	newName = "Prod8ctive.Analytics"
+	fmt.Println("Pointer Address (same) & Content (updated) :")
+	fmt.Println(newOrgNamePtr, *newOrgNamePtr)
 }
